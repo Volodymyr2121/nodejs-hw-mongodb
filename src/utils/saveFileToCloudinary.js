b694ec376a438521385b53424520d73f7191e14d
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2024-09-30 00:03:16 +0200

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import { env } from "./env.js";
+import { env } from "../utils/env.js";
 import * as fs from "node:fs/promises";
 
 const cloud_name = env("CLOUDINARY_CLOUD_NAME");
